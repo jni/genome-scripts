0a772605fbd66158299d1ab789a62e1b1ea7e292
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2013-10-07 15:50:53 +1100

    Fix: BED format is 0-indexed

--- chrcover.py
+++ chrcover.py
@@ -27,6 +27,7 @@ chr_lens = { # hg19 coordinates
     'chrY' :  59373566
 }
 
+
 def get_coverage(bed_fn, lens=chr_lens):
     """Get a set of chromosomes as bool arrays of coverage.
 
@@ -44,14 +45,14 @@ def get_coverage(bed_fn, lens=chr_lens):
     chrs = {}
     n_skipped = 0
     for chr_name in lens:
-        chrs[chr_name] = np.zeros(lens[chr_name] + 1, np.bool)
+        chrs[chr_name] = np.zeros(lens[chr_name], np.bool)
     with open(bed_fn, 'r') as bed:
         for line in bed:
             line = line.split('\t')
             name = line[0]
             start, end = int(line[1]), int(line[2])
             try:
-                chrs[name][start:end+1] = True
+                chrs[name][start:end] = True
             except KeyError:
                 n_skipped += 1
     print n_skipped
